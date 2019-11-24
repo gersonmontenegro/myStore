e75c38860dc598bf219ce663f4aeb8b5be73cb53
Gerson <gersonm@mobilecorp.co>
2019-11-24 13:59:14 -0300

    feat: implement redux to show and change data

--- src/features/detail/index.js
+++ src/features/detail/index.js
@@ -1,23 +1,60 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { connect } from 'react-redux';
+import {
+    Container,
+    Content,
+    Button,
+    Text,
+    Card,
+    CardItem,
+    Icon,
+} from 'native-base';
 import PropTypes from 'prop-types';
+import { setData } from 'actions';
 
 const propTypes = {
     navigation: PropTypes.shape({
         navigate: PropTypes.func,
     }).isRequired,
+    data: PropTypes.number.isRequired,
+    setData: PropTypes.func.isRequired,
 };
 
-const Detail = () => {
+const Detail = (props) => {
+    const { data, setData } = props;
+    const onClickChangeData = () => {
+        setData({ data: data + 1 });
+    };
     return (
-        <SafeAreaView>
-            <View>
-                <Text>Detail!</Text>
-            </View>
-        </SafeAreaView>
+        <Container>
+            <Content>
+                <Card>
+                    <CardItem>
+                        <Button small onPress={onClickChangeData}>
+                            <Icon name="add-circle" />
+                            <Text>Change data!</Text>
+                        </Button>
+                    </CardItem>
+                    <CardItem>
+                        <Text>
+                            Current :
+                            {data}
+                        </Text>
+                    </CardItem>
+                </Card>
+            </Content>
+        </Container>
     );
 };
 
 Detail.propTypes = propTypes;
 
-export default Detail;
+const mapStateToProps = (state) => ({
+    data: state.dataReducer.data,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    setData: (newData) => dispatch(setData(newData)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Detail);
