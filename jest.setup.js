b71f479f7d033ad2ce3c50b25dc5f8814db1af3a
Gerson <gersonm@mobilecorp.co>
2019-11-24 16:05:22 -0300

    feat: add jest config

---
+++ jest.setup.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-labels */
+/* eslint-disable no-labels */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-restricted-syntax */
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
