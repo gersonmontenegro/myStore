63259c0f2f9972ec8f69af66c9cfaf90f7c3c3ce
Gerson <gersonm@mobilecorp.co>
2019-11-24 00:42:53 -0300

    refactor: move App file to src folder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigator from './navigation';
+
+const App: () => React$Node = () => {
+    return (
+        <>
+            <Navigator />
+        </>
+    );
+};
+
+export default App;
