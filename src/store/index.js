e65830a0527ea2653718b249e611de33f0636a3e
Gerson Montenegro <montenegrogerson@gmail.com>
2020-02-04 17:47:45 -0300

    feat: add reactotron to the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,9 @@
-import { createStore } from 'redux';
+/* eslint-disable import/no-unresolved */
+import { createStore, compose } from 'redux';
+import reactotron from 'initStack/ReactotronConfig';
 import reducers from 'reducers';
 
-export default createStore(reducers);
+export default createStore(
+    reducers,
+    compose(reactotron.createEnhancer()),
+);
