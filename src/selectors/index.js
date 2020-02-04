71a8ea801eb976c981ebb0bb354b22f0f8654736
Gerson Montenegro <montenegrogerson@gmail.com>
2020-02-04 17:50:45 -0300

    feat: add selectors
    getproductsbysublevel, getcurrentproductslikes, getcurrentfavorite

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,10 +1,38 @@
 import { createSelector } from 'reselect';
 
-const getData = (state) => state.dataReducer.data;
+const getProducts = (state) => state.products;
+const getCurrentSublevel = (state) => state.currentSublevel.currentSublevel;
+const getCurrentProductId = (state) => state.currentProductId;
 
-const getDataSelector = createSelector(
-    getData,
+const getProductsSelector = createSelector(
+    getProducts,
     (data) => data,
 );
 
-export default getDataSelector;
+const getProductsBySublevel = createSelector(
+    getProducts,
+    getCurrentSublevel,
+    (products, sublevel) => {
+        const data = products.filter((item) => item.sublevel_id === sublevel);
+        return data;
+    },
+);
+
+const getCurrentProductLikes = createSelector(
+    getCurrentProductId,
+    getProducts,
+    (currentId, products) => products.filter((item) => item.id === currentId)[0].likes,
+);
+
+const getCurrentFavorite = createSelector(
+    getCurrentProductId,
+    getProducts,
+    (currentId, products) => products.filter((item) => item.id === currentId)[0].favorite,
+);
+
+export {
+    getProductsSelector,
+    getProductsBySublevel,
+    getCurrentProductLikes,
+    getCurrentFavorite,
+};
