fab114129e5d237cafb0e6331b8a6781eecf643d
Gerson Montenegro <montenegrogerson@gmail.com>
2020-02-06 11:17:42 -0300

    chore: implementing CartItem component

--- src/features/cart/index.js
+++ src/features/cart/index.js
@@ -1,26 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import {
     Container,
     Content,
     Button,
     Text,
-    Left,
     List,
     ListItem,
     Body,
-    Thumbnail,
-    CheckBox,
-    View,
 } from 'native-base';
 import { getCartProductsSelector } from 'selectors';
-import { checkProduct, modifyCart } from 'actions';
-import Utils from 'helpers/utils';
-import Format from 'helpers/format';
-
-const plusIconImage = require('assets/icons/plus.png');
-const minusIconImage = require('assets/icons/minus.png');
+import CartItem from 'components/CartItem';
 
 const propTypes = {
     navigation: PropTypes.shape({
@@ -33,63 +24,9 @@ const propTypes = {
 
 const Cart = (props) => {
     const { navigation } = props;
-    const dispatch = useDispatch();
-    const [chk, setChk] = useState(true);
     const cartProducts = useSelector(getCartProductsSelector);
-    const onCheck = ({ id }) => () => {
-        dispatch(checkProduct({ id }));
-    };
-    const renderCartItem = (item) => {
-        return (
-            <List key={item.id} style={{ right: 5 }}>
-                <ListItem avatar style={{ borderBottomWidth: 1 }}>
-                    <Left>
-                        <Thumbnail source={{ uri: item.thumbnail }} />
-                    </Left>
-                    <Body style={{ borderBottomColor: 'red', borderBottomWidth: 0 }}>
-                        <Text>{item.name}</Text>
-                        <Text note>{Format.currencyFormat(item.price)}</Text>
-                        <View style={{ flexDirection: 'row' }}>
-                            <Button
-                                transparent
-                                onPress={() => {
-                                    const currentValue = Utils.decreaseValue(item.cartQuantity);
-                                    dispatch(modifyCart({ item, operation: '-', quantity: currentValue }));
-                                }}
-                            >
-                                <Thumbnail source={minusIconImage} style={{ width: 25, height: 25 }} />
-                            </Button>
-                            <View style={{ justifyContent: 'center' }}>
-                                <Text>{` ${item.cartQuantity} `}</Text>
-                            </View>
-                            <Button
-                                transparent
-                                onPress={() => {
-                                    const currentValue = Utils.increaseValue(item.cartQuantity, item.quantity);
-                                    dispatch(modifyCart({ item, operation: '+', quantity: currentValue }));
-                                }}
-                            >
-                                <Thumbnail source={plusIconImage} style={{ width: 25, height: 25 }} />
-                            </Button>
-                        </View>
-                        <View style={{ flexDirection: 'row' }}>
-                            <Text>
-                                {
-                                    `Subtotal: ${Format.currencyFormat(item.cartQuantity * item.price)}`
-                                }
-                            </Text>
-                        </View>
-                    </Body>
-                    <CheckBox
-                        checked={item.check}
-                        onPress={onCheck(item)}
-                    />
-                </ListItem>
-            </List>
-        );
-    };
     const renderCartProducts = (products) => {
-        return products.map((item) => renderCartItem(item));
+        return products.map((item) => <CartItem key={item.key} item={item} />);
     };
     return (
         <Container style={{ top: 20 }}>
