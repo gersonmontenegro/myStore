35ac5ecacf60edcd3915afd21a34aa613f6e0767
Gerson <gersonm@mobilecorp.co>
2020-02-06 08:00:11 -0300

    chore: create render item in shopping cart

--- src/features/cart/index.js
+++ src/features/cart/index.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import map from 'lodash/map';
 import { useDispatch, useSelector } from 'react-redux';
 import {
     Container,
     Content,
-    Header,
-    Footer,
     Button,
     Text,
-    FooterTab,
     Left,
-    Icon,
     List,
     ListItem,
     Body,
+    Thumbnail,
+    CheckBox,
 } from 'native-base';
 import { getCartProductsSelector } from 'selectors';
-import { getProductDetailById } from 'actions';
+import { checkProduct } from 'actions';
+import Format from 'helpers/format';
 
 const propTypes = {
     navigation: PropTypes.shape({
@@ -31,14 +30,32 @@ const propTypes = {
 const Cart = (props) => {
     const { navigation } = props;
     const dispatch = useDispatch();
+    const [chk, setChk] = useState(true);
     const cartProducts = useSelector(getCartProductsSelector);
-    const renderCartProducts = (items) => {
-        return map(items, async (quantity, id) => {
-            console.log('>>');
-            const itemDetail = await dispatch(getProductDetailById(id));
-            console.log('>', itemDetail);
-            return <Text>{id}</Text>;
-        });
+    const onCheck = ({ id }) => () => {
+        dispatch(checkProduct({ id }));
+    };
+    const renderCartItem = (item) => {
+        return (
+            <List key={item.id} style={{ right: 5 }}>
+                <ListItem avatar>
+                    <Left>
+                        <Thumbnail source={{ uri: item.thumbnail }} />
+                    </Left>
+                    <Body>
+                        <Text>{item.name}</Text>
+                        <Text note>{Format.currencyFormat(item.price)}</Text>
+                    </Body>
+                    <CheckBox
+                        checked={item.check}
+                        onPress={onCheck(item)}
+                    />
+                </ListItem>
+            </List>
+        );
+    };
+    const renderCartProducts = (products) => {
+        return products.map((item) => renderCartItem(item));
     };
     return (
         <Container style={{ top: 20 }}>
