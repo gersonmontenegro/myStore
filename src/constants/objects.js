30ba92f5a99b78e0713704bdac3d57cca0527c0d
Gerson <gersonm@mobilecorp.co>
2020-02-09 04:57:36 -0300

    feat: add objects file to keep general objects

---
+++ src/constants/objects.js
@@ -0,0 +1,14 @@
+const conditionalSortOptions = [
+    {
+        firstCondition: -1,
+        secondCondition: 1,
+    },
+    {
+        firstCondition: 1,
+        secondCondition: -1,
+    },
+];
+
+export default {
+    conditionalSortOptions,
+};
