138ab536a431b826b0ebb4bff9f689437f8d3621
Gerson Montenegro <montenegrogerson@gmail.com>
2020-02-11 11:41:32 -0300

    chore: test list products

--- src/features/cart/index.spec.js
+++ src/features/cart/index.spec.js
@@ -1,6 +1,9 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { cleanup, render } from 'react-native-testing-library';
+import Format from 'helpers/format';
+import { useSelector } from 'react-redux';
+import { getCartProductsSelector, getTotalFromCart } from 'selectors';
 import Cart from './index';
 
 const props = {
@@ -27,4 +30,8 @@ describe('Test cart interface', () => {
     });
 
     test('should render # elements', () => {
+        const { getAllByType } = wrapper;
+        const totalItems = getAllByType('Image');
+        expect(totalItems).toHaveLength(getCartProductsSelector.length * 3);// just because there are 3 images by item
+    });
 });
